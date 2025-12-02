2580e87c5cb8609a4118a6c0bc4d5fe9000445a6
Aaron <aaronson7905@gmail.com>
2025-12-02 10:37:26 +0100

    sauvegarde avant merge

--- SAE/backend/src/Controller/AnimalController.php
+++ SAE/backend/src/Controller/AnimalController.php
@@ -6,10 +6,13 @@ use App\Repository\AnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/api', name: 'api_')]
 class AnimalController extends AbstractController
 {
+    // GET
     #[Route('/animals', name: 'animals_list', methods: ['GET'])]
     public function list(AnimalRepository $animalRepository): JsonResponse
     {
@@ -74,5 +77,75 @@ class AnimalController extends AbstractController
         }, $animals);
         
         return $this->json($data);
+    }
+    // PUT
+    #[Route('/animals/{id}', name: 'animal_update', methods: ['PUT'])]
+    public function update(
+        int $id,
+        Request $request,
+        AnimalRepository $animalRepository,
+        EntityManagerInterface $entityManager,        
+    ): JsonResponse
+    
+    {
+        $animal = $animalRepository->find($id);
+        
+        if (!$animal) {
+            return $this->json(['error' => 'Animal not found'], 404);
+        }
+        
+        $data = json_decode($request->getContent(), true);
+        
+        if (isset($data['commonName'])) {
+            $animal->setCommonName($data['commonName']);
+        }
+        if (isset($data['scientificName'])) {
+            $animal->setScientificName($data['scientificName']);
+        }
+        if (isset($data['family'])) {
+            $animal->setFamily($data['family']);
+        }
+        if (isset($data['type'])) {
+            $animal->setType($data['type']);
+        }
+        if (isset($data['extinctLevel'])) {
+            $animal->setExtinctLevel($data['extinctLevel']);
+        }
+        if (isset($data['images'])) {
+            $animal->setImage($data['images']);
+        }
+        
+        $entityManager->flush();
+        
+        return $this->json([
+            'message' => 'Animal updated successfully',
+            'animal' => [
+                'id' => $animal->getId(),
+                'commonName' => $animal->getCommonName(),
+                'scientificName' => $animal->getScientificName(),
+                'family' => $animal->getFamily(),
+                'type' => $animal->getType(),
+                'extinctLevel' => $animal->getExtinctLevel(),
+                'images' => $animal->getImage(),
+            ],
+        ]);
+    }
+    //DELETE
+    #[Route('/animals/{id}', name: 'animal_delete', methods: ['DELETE'])]
+    public function delete(
+        int $id,
+        AnimalRepository $animalRepository,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+        $animal = $animalRepository->find($id);
+        
+        if (!$animal) {
+            return $this->json(['error' => 'Animal not found'], 404);
+        }
+        
+        $entityManager->remove($animal);
+        $entityManager->flush();
+        
+        return $this->json(['message' => 'Animal deleted successfully']);
     }
 }
